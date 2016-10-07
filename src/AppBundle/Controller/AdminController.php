7e2a72b528c1f085f7bb2e21b3d6cf7b6a57aec2
Wilmer Ramones <wramones@ubv.edu.ve>
2016-10-07 11:02:33 -0400

    creado el controlador del administrador del LMS

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Copyright (C) 2016 Tecnolínea Paraguná C.A.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of AdminController
+ *
+ * @author Tecnolínea Paraguná C.A.
+ * 
+ * @Route("/admin")
+ */
+class AdminController extends Controller {
+    
+    /**
+     * @Route("/", name="admin_homepage")
+     */
+    public function indexAction(Request $request){
+        
+        return $this->render('admin/index.html.twig');
+        
+    }
+    
+    
+}
