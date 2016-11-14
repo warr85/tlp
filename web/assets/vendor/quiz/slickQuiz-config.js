e40db91d55077e9723f018d4ab984da5c9c3d88d
Wilmer Ramones <wramones@ubv.edu.ve>
2016-11-14 15:58:13 -0400

    creando la revista para explicar la historia del nacimiento de GIT

--- web/assets/vendor/quiz/slickQuiz-config.js
+++ web/assets/vendor/quiz/slickQuiz-config.js
@@ -4,68 +4,91 @@
 
 var quizJSON = {
     "info": {
-        "name":    "Test Your Knowledge!!",
-        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
-        "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
-        "level1":  "Jeopardy Ready",
-        "level2":  "Jeopardy Contender",
-        "level3":  "Jeopardy Amateur",
-        "level4":  "Jeopardy Newb",
-        "level5":  "Stay in school, kid..." // no comma here
+        "name":    "Enciende Tu Conocimiento!!",
+        "main":    "<p>Vamos a averiguar todo lo que hemos encendido a través de este rápido quiz</p>",
+        "results": "<h5>Saber más:</h5><p>Puedes expandir tu conocimiento a través del manual de git en español a través del siguiente enlace: <a href='#'>enlace</a>.</p>",
+        "level1":  "Conocedor de GIT",
+        "level2":  "Nociones de GIT",
+        "level3":  "Iniciado en GIT",
+        "level4":  "Novato en GIT",
+        "level5":  "Es mejor volver a repasar las clases de GIT" // no comma here
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "¿Qué es GIT?",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "Un Lenguaje de Programación",      "correct": false},
+                {"option": "Un Gestor de Base de Datos",     "correct": false},
+                {"option": "Un Controlador de Versiones Distribuido",      "correct": true},
+                {"option": "Una Banda de música",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>Correcto!</span> Git es un DVCS!</p>",
+            "incorrect": "<p><span>Uhh no.</span> GIT es un controlador de versiones DISTRIBUIDO </p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Which of the following best represents your preferred breakfast?",
+            "q": "CVS only tracks changes to single files.  What does Git do??",
             "a": [
                 {"option": "Bacon and eggs",               "correct": false},
-                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
+                {"option": "git tracks entire source trees with a global view",   "correct": true},
                 {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "Eggs, fruit, toast, and milk", "correct": false} // no comma here
             ],
-            "select_any": true,
+            //"select_any": true,
             "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
             "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Where are you right now? Select ALL that apply.",
+            "q": "To set your email address at the global--or user--level, what command would you type?.",
             "a": [
-                {"option": "Planet Earth",           "correct": true},
-                {"option": "Pluto",                  "correct": false},
-                {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "git set --user email <email>",           "correct": false},
+                {"option": "git config --global user.email <email>", "correct": true},
+                {"option": "git config email <email>",  "correct": false},
+                {"option": "git set --user user.email>email", "correct": false} // no comma here
             ],
             "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
             "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
         },
         { // Question 4
-            "q": "How many inches of rain does Michigan get on average per year?",
+            "q": "Versionar tu código te permite...?",
             "a": [
-                {"option": "149",    "correct": false},
-                {"option": "32",     "correct": true},
-                {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "Trabajar en equipo en el mismo proyecto",    "correct": true},
+                {"option": "Tener una lista de modificaciones del código",     "correct": true},
+                {"option": "Validar automáticamente la funcionalidad del código de otro desarrollador",      "correct": false},
+                {"option": "Conocer quién escribió una linea de código",   "correct": true} // no comma here
             ],
+            "select_any": true,
             "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
             "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
         },
         { // Question 5
-            "q": "Is Earth bigger than a basketball?",
+            "q": "¿Cuál es la razón por la que Git puede ser más rápido que otros sistemas?",
             "a": [
-                {"option": "Yes",    "correct": true},
+                {"option": "El historial del código es local.",    "correct": false},
+                {"option": "El historial del código está en la nube.",     "correct": false},
+                {"option": "El código no se utiliza completamente.",      "correct": false},
+                {"option": "El historial del código está en más de una máquina.",   "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
+            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
+        },
+        { // Question 6
+            "q": "Hay otras opciones para controlar versiones a parte de GIT?",
+            "a": [
+                {"option": "Si",    "correct": true},
                 {"option": "No",     "correct": false} // no comma here
             ],
             "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
+            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+        },
+        { // Question 7
+            "q": "¿En que año Nació GIT?",
+            "a": [
+                {"option": "1980",    "correct": false},
+                {"option": "2005",    "correct": true},
+                {"option": "2000",    "correct": false},
+                {"option": "2015",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correcto!</span> You must be very observant!</p>",
             "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
         } // no comma here
     ]
