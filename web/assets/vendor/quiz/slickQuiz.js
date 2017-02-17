c677ee82dc7c3c8c7b695b79fb58e12ef3b2c410
Wilmer Ramones <wilmer.ramones@gmail.com>
2017-02-17 11:02:11 -0400

    reemplazado el toastr por el izitoast en slick quiz.

--- web/assets/vendor/quiz/slickQuiz.js
+++ web/assets/vendor/quiz/slickQuiz.js
@@ -464,7 +464,12 @@
                 });
 
                 if (plugin.config.preventUnanswered && selectedAnswers.length === 0) {
-                    toastr.error("Debes seleccionar al menos una respuesta","Error");           
+		           iziToast.error({                                                
+		                  title: 'Error',
+		                  message: 'Debes seleccionar al menos una respuesta',		                  
+		                  position: 'center'
+		              });
+                              
                     return false;
                 }
 
@@ -600,7 +605,12 @@
                 key = internal.method.getKey (1); // how many notches == how many jQ animations you will run
                 keyNotch = internal.method.getKeyNotch; // a function that returns a jQ animation callback function
                 kN = keyNotch; // you specify the notch, you get a callback function for your animation
-                toastr.info("Felicidades, has terminado el QUIZ y has conseguido todos los logros de este nivel.",'Quiz Terminado!');
+                iziToast.success({                                                
+		                  title: 'Felicitaciones',
+		                  message: 'has terminado el QUIZ y has conseguido todos los logros de este nivel.',		                  
+		                  position: 'center'
+		              });
+                
                 $("logro_quiz").removeClass("btn-outline").addClass("btn-success-done animated rubberBand");
                 $nextClass.show();
                 var score        = $(_element + ' ' + _correct).length,
@@ -633,7 +643,11 @@
                     $(_quizLevel).addClass('level' + levelRank);
                     alert(levelRank);
                     if (levelRank < 3){
-                        toastr.info("Felicidades, has terminado el QUIZ y has conseguido todos los logros de este nivel.",'Quiz Terminado!');
+                        iziToast.success({                                                
+		                  title: 'Felicitaciones',
+		                  message: 'has terminado el QUIZ y has conseguido todos los logros de este nivel.',		                  
+		                  position: 'center'
+		              }); 
                         $('.progress-bar').css('width', '100%');
                         $('.stat-percent').html('100%');
                         $('.progress-bar').attr('aria-valuenow', '100');
@@ -735,7 +749,12 @@
 
                          });
                     }else if (levelRank < 4 ){
-                        toastr.warning("Quiz terminado, debes practicar un poco mas, pero puedes avanzar.",'Quiz Terminado!');
+                    iziToast.warning({                                                
+		                  title: 'Quiz terminado',
+		                  message: 'debes practicar un poco mas, pero puedes avanzar.',		                  
+		                  position: 'center'
+		              }); 
+                        
                         $('.progress-bar').css('width', '100%');
                         $('.stat-percent').html('100%');
                         $('.progress-bar').attr('aria-valuenow', '100');
@@ -775,7 +794,11 @@
 
                          });
                     }else{
-                         toastr.error("Debemos repasar nuevamente los conceptos que rodean GIT.",'Debemos intentar nuevamente!');                        
+                     iziToast.error({                                                
+		                  title: 'Debemos intentar nuevamente',
+		                  message: 'hay que repasar nuevamente los conceptos que rodean GIT',		                  
+		                  position: 'center'
+		              });                          
                          $quizResultsCopy.append('<p><a class="button ' + tryAgainClass + '" href="#">' + plugin.config.tryAgainText + '</a></p>');
                     }
                 }
