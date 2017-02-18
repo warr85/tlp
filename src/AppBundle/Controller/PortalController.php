635b5328d155a910b6d84795b6098e01ebdce473
Wilmer Ramones <wilmer.ramones@gmail.com>
2017-02-18 17:08:33 -0400

    cambiando de frameworkextrabundle a symfony routing en el portal

--- src/AppBundle/Controller/PortalController.php
+++ src/AppBundle/Controller/PortalController.php
@@ -2,10 +2,9 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Usuarios;
 
 class PortalController extends Controller
