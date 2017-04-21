66971e13cd8f5cbd5d262003c08bbfef536984e8
Wilmer <wilmer.ramones@gmail.com>
2017-04-21 10:21:15 -0400

    termina el quiz del tema 5 del curso de Git

--- web/assets/vendor/quiz/slickQuiz-config5.js
+++ web/assets/vendor/quiz/slickQuiz-config5.js
@@ -5,7 +5,7 @@
 var quizJSON = {
     "info": {
         "name":    "Quinto Quiz",
-        "main":    "<p>Vamos a encender nuestros conocimientos a través de este 4to presente Quiz.  El mismo estará disponible una vez hayas aprendido la arquitectura de Git</p>",
+        "main":    "<p>Vamos a encender nuestros conocimientos a través de este 5to Quiz.  El mismo estará disponible una vez hayas aprendido la arquitectura de Git</p>",
         "results": "<h5>Saber más:</h5><p>Puedes expandir tu conocimiento a través del manual de git en español a través del siguiente enlace: <a href='#'>enlace</a>.</p>",
         "level1":  "Conocedor de GIT",
         "level2":  "Nociones de GIT",
@@ -15,47 +15,48 @@ var quizJSON = {
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "¿Que sucede si ejecutamos el comando git log sin ninguna opción?",
+            "q": "¿Que sucede si ejecutamos el comando git diff sin ninguna opción y tenemos archivos modificados en el working y en el stage?",
             "a": [
-                {"option": "muestra el estatus del proyecto",      "correct": false},
-                {"option": "muestra la confirmación mas reciente",     "correct": false},
-                {"option": "muestra todas las confirmaciones cronologicamente inversa",      "correct": true},
-                {"option": "muestra un error en la pantalla",     "correct": false} // no comma here
+                {"option": "nos muestra todo lo que ha cambiado",      "correct": false},
+                {"option": "no muestra nada",     "correct": false},
+                {"option": "solo muestra lo que ha cambiado en el Stage",      "correct": false},
+                {"option": "Solo muestra lo que ha cambiado en el Working",     "correct": true} // no comma here
             ],
-            "correct": "<p><span>Correcto!</span> Correcto: El comando git log sin opciones muestra todo el historial empezando desde el mas reciente hasta el primer commit realizado en el proyecto.</p>",
-            "incorrect": "<p><span>Uhh no.</span> El comando git log sin opciones muestra todo el historial empezando desde el mas reciente hasta el primer commit realizado en el proyecto. </p>" // no comma here
+            "correct": "<p><span>Correcto!</span> Correcto: El comando git diff sin opciones muestra todo las diferencias que tienen los archivos entre el repositorio y el working.</p>",
+            "incorrect": "<p><span>Uhh no.</span> El comando git diff sin opciones muestra todo las diferencias que tienen los archivos entre el repositorio y el working. </p>" // no comma here
         },
         { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "¿Es cierto que podemos modificar lo que muestra el comando git log?",
+            "q": "¿Podemos ver los cambios que hay entre el Stage y el Repositorio?",
             "a": [
                 {"option": "Verdadero",               "correct": true},
                 {"option": "Falso",   "correct": false}
             ],
             //"select_any": true,
-            "correct": "<p><span>Excelente!</span> Con las opciones y los parametros podemos tener el control de lo que muestra y como lo muestra</p>",
-            "incorrect": "<p><span>Hmmm.... hay que repasar</span> recuerda la parte de los parametros y opciones.</p>" // no comma here
+            "correct": "<p><span>Excelente!</span> Con la opción --staged podemos ver esas diferencias</p>",
+            "incorrect": "<p><span>Hmmm.... hay que repasar</span> recuerda que Con la opción --staged podemos ver esas diferencias.</p>" // no comma here
         },
         { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "¿Puedo ejecutar varios parametros a la vez?",
+            "q": "¿Que sucede si el repositorio está limpio y ejecutamos git diff?",
             "a": [
-                {"option": "Cierto",           "correct": true},
-                {"option": "Falso'", "correct": false}
+                {"option": "muestra los ultimos cambios realizados",           "correct": false},
+                {"option": "no muestra nada'", "correct": true},
+                {"option": "ninguna de las anteriores'", "correct": false}
 
             ],
-            "correct": "<p><span>Brillante!</span> con varios parametros te aseguras que lo que estas buscando se te sea mostrado como lo necesitas</p>",
-            "incorrect": "<p><span>Incorrecto.</span> Puedes colocar tantos parametros como necesites para encontrar lo que buscas.</p>" // no comma here
+            "correct": "<p><span>Brillante!</span> El comando solo muestra las diferencias entre el Repositorio y el Working, de no haber diferencias, no muestra nada</p>",
+            "incorrect": "<p><span>Incorrecto.</span> Recuerda que comando solo muestra las diferencias entre el Repositorio y el Working, de no haber diferencias, no muestra nada.</p>" // no comma here
         },
         { // Question 4
-            "q": "Si quiero que me aparezcan las confirmaciones desde una fecha hasta el presente, ¿Cuál sería el paremetro?",
+            "q": "¿Que sucede si el repositorio está limpio y ejecutamos git diff?",
             "a": [
-                {"option": "--author",           "correct": false},
-                {"option": "--since",           "correct": true},
-                {"option": "--grep",           "correct": false},
-                {"option": "--until",     "correct": false} // no comma here
+                {"option": "muestra los ultimos cambios realizados",           "correct": false},
+                {"option": "Muestra lo ultimo añadido al stage", "correct": false},
+                {"option": "ninguna de las anteriores'", "correct": true}
+
             ],
             //"select_any": true,
-            "correct": "<p><span>WOW!</span> Felicidades! Tienes el control y el super poder de controlar la historia del proyecto</p>",
-            "incorrect": "<p><span>Falla.</span> te falta repasar el camino para adquirir este super poder.!</p>" // no comma here
+            "correct": "<p><span>WOW!</span> Felicidades! Para responder estas preguntas debes tener un gran conocimiento sobre la estructura y funcionamiento de GIT</p>",
+            "incorrect": "<p><span>Falla.</span> Se que es un poco confuso, pero respansando lo vamos a lograr.!</p>" // no comma here
         }
     ]
 };
